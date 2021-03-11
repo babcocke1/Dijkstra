b86bd731228e8e2812e62f3de98299c4f7040a8c
babcocke1 <56487953+babcocke1@users.noreply.github.com>
2021-03-11 02:42:31 -0700

    added assets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Node : MonoBehaviour
+{
+    public List<Node> connectedTo;
+    public GameObject nodeObject;
+
+    void Start()
+    {
+        nodeObject.transform.position = nodeObject.transform.position + new Vector3(Random.value - .5f, 0f, Random.value - .5f);
+    }
+}
